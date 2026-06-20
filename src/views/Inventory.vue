7abdb990e741f629cd83e0db1419fd5391f32b98
Vaibhavi Upreti <vaibhavi.upreti@hotwax.co>
2026-06-20 14:39:29 +0530

    added 2 changes, 1-footer buttons are visible yet disabled until some product is selected, 2-added padding-right to enhance UI

--- src/views/Inventory.vue
+++ src/views/Inventory.vue
@@ -87,11 +87,11 @@
         </template>
       </ion-list>
     </ion-content>
-    <ion-footer v-if="isAnyProductSelected">
+    <ion-footer>
       <ion-toolbar class="footer-actions">
         <ion-buttons>
-          <ion-button @click="openBulkInventoryEditModal">{{ "Adjust Inventory" }}</ion-button>
-          <ion-button @click="openProductFacilityConfigModal()">{{ "Adjust Config" }}</ion-button>
+          <ion-button :disabled="!isAnyProductSelected" @click="openBulkInventoryEditModal">{{ "Adjust Inventory" }}</ion-button>
+          <ion-button :disabled="!isAnyProductSelected" @click="openProductFacilityConfigModal()">{{ "Adjust Config" }}</ion-button>
         </ion-buttons>
       </ion-toolbar>
     </ion-footer>
@@ -228,6 +228,7 @@ ion-content {
   --columns-desktop: 6;
   border-bottom : 1px solid var(--ion-color-medium);
   align-items: center;
+  padding-inline-end: var(--spacer-base, 16px);
 }
 
 .list-item > ion-item {
